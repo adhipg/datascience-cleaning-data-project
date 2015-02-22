1f300cb30819ba1b683d06ca1bc2549f9bd0e49e
Adhip Gupta <me@adhipg.in>
2015-02-23 03:57:07 +0530

    Create processedData.txt and tidy.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,13 @@
-# This file does the following:
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names.
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-##
+# Gets all the data and runs the analysis to create two files:
+# 1. processedData.txt -> raw processed data.
+# 2. tidy.txt -> summarised data (mean) by subject and activity.
 
 # Step 0
 ########
-# Download the CSV file if needed.
-
-localPath <- "UCI HAR Dataset"
+# Setup
+# location of the data with respect to the current working directory. Change
+# this to . if run_analysis.R is in the same file as the data directory.
+localPath <- "UCI HAR Dataset/"
 
 # Step 1
 ########
@@ -21,10 +18,10 @@ localPath <- "UCI HAR Dataset"
 # 3. Merge the two data-sets into a single data set.
 
 getCombinedDataSet <- function( dataType ) {
-  dataX <- read.table(paste(localPath, "/", dataType, "/X_", dataType, ".txt", sep=""))
-  dataY <- read.table(paste(localPath, "/", dataType, "/Y_", dataType, ".txt",
+  dataX <- read.table(paste(localPath, dataType, "/X_", dataType, ".txt", sep=""))
+  dataY <- read.table(paste(localPath, dataType, "/Y_", dataType, ".txt",
                             sep=""), col.names=c("activityId"))
-  dataSubject <- read.table(paste(localPath, "/", dataType, "/subject_", dataType, ".txt",
+  dataSubject <- read.table(paste(localPath, dataType, "/subject_", dataType, ".txt",
                             sep=""), col.names=c("subjectId"))
   dataSet <- cbind(dataX, dataY, dataSubject)
   dataSet
@@ -40,7 +37,7 @@ dataSet <- rbind(trainingData, testData)
 # measurement.
 # The col-names for the measurements are in features.txt
 # Apart from the means and std, we also want to keep the subject and activityId
-features <- read.table(paste(localPath, "/features.txt", sep=""))
+features <- read.table(paste(localPath, "features.txt", sep=""))
 desiredFeatureCols <- grep("-(mean|std)\\(\\)", features[, 2])
 desiredFeatures <- features[desiredFeatureCols, ]
 # Add the column numbers for activityId and subjectId
@@ -53,9 +50,10 @@ dataSet <- dataSet[, desiredFeatureCols]
 ########
 # Use descriptive activity names to name the activities in the data set
 # The activity names are given in activity_labels.txt
-activities <- read.table(paste(localPath, "/activity_labels.txt", sep=""),
+activities <- read.table(paste(localPath, "activity_labels.txt", sep=""),
                          col.names=c("activityId", "activity_label"))
 dataSet <- merge(dataSet, activities)
+dataSet$activityId = NULL
 
 # Step 4
 ########
@@ -69,14 +67,17 @@ desiredFeatures[, 2] <- gsub("-", "", desiredFeatures[, 2])
 desiredFeatures[, 2] <- gsub("BodyBody", "Body", desiredFeatures[, 2])
 
 niceColNames <- desiredFeatures[, 2]
-niceColNames <- c("activityId", niceColNames, "SubjectId", "Activity")
+niceColNames <- c(niceColNames, "Subject", "Activity")
 colnames(dataSet) <- niceColNames
-dataSet$activityId = NULL
+
+write.table(dataSet, "processedData.txt", sep="\t", row.name=FALSE)
 
 # Step 5
 #######
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
 
-print(head(dataSet, 2))
-print(nrow(dataSet))
-print(ncol(dataSet))
+library(plyr)
+
+tidySet <- ddply(dataSet, c("Subject", "Activity"), numcolwise(mean))
+write.table(tidySet, "tidy.txt", sep="\t", row.name=FALSE)
